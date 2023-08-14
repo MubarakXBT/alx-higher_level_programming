748f62233d4f41c52d7b7b21ba75089497347fec
root <kazeemmubarak@gmail.com>
2023-08-14 18:55:14 +0000

    function checkes last digit of given number and tells if ky is 0, greater or lesser than[fix for pycodestyle]

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,17 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number < 0:
-    num = number * -1
-    mod = num % 10
-else:
-    mod = number % 10
 if (number >= 0):
+    mod = number % 10
     if mod > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, mod))
+        print(f"Last digit of {number} is {mod} and is greater than 5")
     elif mod == 0:
         print("Last digit of {} is {} and is 0".format(number, mod))
     else:
         print(f"Last digit of {number} is {mod} and is less than 6 and not 0")
 else:
-        print(f"Last digit of {number} is -{mod} and is less than 6 and not 0")
+    num = number * -1
+    mod = num % 10
+    print(f"Last digit of {number} is -{mod} and is less than 6 and not 0")
