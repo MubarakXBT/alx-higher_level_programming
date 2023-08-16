355a84cdcf031d1efc9c430daafedef132cb34f3
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-16 15:35:22 +0000

    Function to change lowercase chr in a string to uppercase[1-print fix]

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,17 +2,19 @@
 def uppercase(str):
     i = 0
     j = 97
+    new_str = ""
     while i < len(str):
         if j != 123 and j != ord(str[i]):
             j = j + 1
             continue
         elif j < 123 and j == ord(str[i]):
             j = j - 32
-            print("{:c}".format(j), end="")
-            j == 97
+            new_str = new_str + chr(j)
+            j = 97
             i = i + 1
         elif j == 123:
-            print("{}".format(str[i]), end="")
+            new_str = new_str + str[i]
             i = i + 1
             j = 97
-    print()
+    print(new_str)
+
