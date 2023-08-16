a6d9374edcdb5b2bae2d64e7611cfb74e81e8769
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-16 15:51:39 +0000

    Function return last_digit of given number[Fix for missing print]

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-        return (number % 10)
+        value = number % 10
     else:
         number = number * -1
-        return (number % 10)
+        value = number % 10
+    print(value, end="")
+    return (value)
