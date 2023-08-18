1b2bb02d6577175149a613b35edfb09d56adc6ca
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-18 11:52:09 +0100

    Project  Data Structures: Lists, Tuples - Task 4 [a function that prints all integers of a list, in reverse order. --FIX - Empty list]

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        exit (1)
     count = len(my_list) - 1
     while count >= 0:
         print("{:d}".format(my_list[count]))
