94d8c33ce227f8c33af44ca9465d879be762c726
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-19 21:54:18 +0100

    Project 0x03: Task 6[Function that prints matrix of integers - Fix for output length]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if matrix != [[]]:
         for row in matrix:
             for i in range(3):
-                print("{:d}".format(row[i]), end=' ')
+                if i == 2:
+                    print("{:d}".format(row[i]),end='')
+                else:
+                    print("{:d}".format(row[i]), end=' ')
             print("".format())
     else:
         print("".format())
