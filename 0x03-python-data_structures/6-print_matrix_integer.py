a3822d0ea68e41fa2760dd4ea42b40fd2adb14e9
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-19 22:18:56 +0100

    Project 0x03: Task 6[Function that prints matrix of integers - Fix for Pycodestyle]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix != [[]]:
         for row in matrix:
-            l = len(row)
-            break
-        for row in matrix:
-            for i in range(l):
-                if i == (l - 1):
-                    print("{:d}".format(row[i]),end='')
+            for i in range(len(row)):
+                if i == (len(row) - 1):
+                    print("{:d}".format(row[i]), end='')
                 else:
                     print("{:d}".format(row[i]), end=' ')
             print("".format())
