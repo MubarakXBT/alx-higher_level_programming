2e8804110d1a68f2febac83a11d49051668488c8
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-20 15:28:35 +0100

    Project 0x04: Task 11 [Teachnical Interview - Create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.]

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    lib = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string is None or not (isinstance(roman_string, str)):
+        return (0)
+    else:
+        lst = []
+        for name in roman_string:
+            for k, v in lib.items():
+                if name == k:
+                    lst.append(v)
+                    break
+        lst.append(0)
+        j = 1
+        i = 0
+        result = 0
+        while i < len(lst) :
+            if j <= len(roman_string):
+                if lst[i] < lst[j]:
+                    result += lst[j] - lst[i]
+                    i += 2
+                    j += 2
+                else:
+                    result += lst[i]
+                    j += 1
+                    i += 1
+            else:
+                break
+        return (result)
