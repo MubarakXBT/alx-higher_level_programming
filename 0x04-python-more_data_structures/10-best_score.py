182e70667e95ad91768a370b33ad1bdc28065524
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-20 13:38:02 +0100

    Project 0x04: Task 9 [Function That return key with biggest integer value]

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    i = 0
+    if a_dictionary is None:
+        return (None)
+    for k, v in a_dictionary.items():
+        if v > i:
+            i = v
+            j = k
+    return (j)
