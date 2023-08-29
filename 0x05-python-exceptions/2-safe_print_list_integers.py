cf3cdbc3284f9bc12182c6cef002ea56a718133f
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-29 05:34:21 +0100

    function to try print integer with{:d}.format() --FIX - IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             ln += 1
         except (TypeError, ValueError):
             continue
-        except (IndexError):
-            return
     print()
     return (ln)
 
