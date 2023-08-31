00a4ea71f39a0674d642820fc8928cf062c56e2f
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-31 22:38:40 +0100

    project OOP - Task 1[initialize the  class Square that with attribute size]

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define class square"""
+
+
+class Square:
+    """ represent square"""
+    def __init__(self, size):
+        """ initialize the square
+
+        Args:
+            size: size of the new square
+        """
+        self.__size = size
