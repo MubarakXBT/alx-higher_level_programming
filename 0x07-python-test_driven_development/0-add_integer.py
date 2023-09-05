10e3e512497a055cedcdca36edd5efb76f67bfcc
MubarakXBT <kazeemmubarak@gmail.com>
2023-09-05 14:06:38 +0100

    Project 0x07 - Test Development[Task 0] *A project that that adds given 2 arguments with a usage documentation

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+        if ((not isinstance(a, float) and  not isinstance(a, int))):
+             raise TypeError("a must be an integer")
+        if ((not isinstance(b, float) and  not isinstance(b, int))):
+             raise TypeError("b must be an integer")
+        if isinstance(a, float):
+              a = int(a)
+        if isinstance(b, float):
+              b = int(b)
+        return (a + b)
