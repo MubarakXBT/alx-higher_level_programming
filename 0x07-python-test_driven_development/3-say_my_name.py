bd4c66d0545f69aff8376e82f3c4104e95562b2f
MubarakXBT <kazeemmubarak@gmail.com>
2023-09-05 17:57:02 +0100

    Project 0x07 - Test Development[Task 2] *A function that that says the given name *with a doctest [unit testing] documentation

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" a module for performs task on name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ say_my_name - A function to send name to STDOUT_FILENO
+    Args:
+        first_name (str): first name of the individual
+        last_name (str): Last name of the individual
+    Returns:
+        prints the name to standard outut
+    """
+    if not isinstance(first_name, str) or first_name.isdigit():
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str) or last_name.isdigit():
+        raise TypeError("last_name must be a string")
+    else:
+        print(first_name + ' ' + last_name)
+
+if __name__ == "__main__":
+    say_my_name(None, "Kazeem")
