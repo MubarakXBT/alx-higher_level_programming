27b161113d573dd959d348393ccf0106324c3560
MubarakXBT <kazeemmubarak@gmail.com>
2023-09-13 20:44:07 +0100

    projrct  0x0A - [Inheritance] ========================== Task 10: A class Square - that inherit Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,6 +14,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
+        """computes area of the square"""
         return (self.__size ** 2)
