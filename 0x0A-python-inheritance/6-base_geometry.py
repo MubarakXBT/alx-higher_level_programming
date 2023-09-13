180ee4e2e547a0d700b616377806495c460e2ad7
MubarakXBT <kazeemmubarak@gmail.com>
2023-09-13 17:51:28 +0100

    projrct  0x0A - [Inheritance] ========================== Task 6: The class BaseGeometry with a public inst method Area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A module of Geometry"""
+
+
+class BaseGeometry:
+    """"A class BaseGeometry"""
+    def area(self):
+        """ area - a function to compute area of the shape"""
+        raise Exception("area() is not implemented")
