dc681ff415cb7369381a63845ad0bf82323db369
MubarakXBT <kazeemmubarak@gmail.com>
2023-12-05 14:59:42 +0100

    task 01 - A module to write to text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module to write to file"""
+def write_file(filename="", text=""):
+    """ function to write to the file
+    Args:
+		@filename: file where text is to be stored
+        @text: string to be inserted or appended to the file
+    """
+    with open(filename, mode="w", encoding="utf-8") as myfile:
+        return(myfile.write(text))
