6fc2408a10acd6c07d15f9700e26d11a0c6bf0d8
MubarakXBT <kazeemmubarak@gmail.com>
2023-12-05 14:39:11 +0100

    task 01 - A module that reads from file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A module to open and read a file to STDOUT"""
+def read_file(filename=""):
+    """ reads file to stdout.
+    Args:
+		filename: name of the file, text is to be read from.
+    Returns:
+		Null.
+    """
+    with open(filename, mode="r", encoding="utf-8") as myfile:
+        {
+            print(myfile.read(), end="")
+		}
