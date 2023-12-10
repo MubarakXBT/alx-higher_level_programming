ea118dd6339833acc024710022b30fc2f0669091
MubarakXBT <kazeemmubarak@gmail.com>
2023-12-10 16:47:32 +0100

    Task: Create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module JSON """
+import json
+
+
+def load_from_json_file(filename):
+    """ A function to decode json from a file
+    Args:
+        filename: JSON file that contains the representation
+    Return:
+        an Object from the JSON file
+    """
+    with open(filename, mode="r", encoding="utf-8") as jsonfile:
+        return (json.loads(jsonfile.read()))
