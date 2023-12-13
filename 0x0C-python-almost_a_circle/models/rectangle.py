cf2ea9ba9ec184ffb5fad1b339013e1a3800491b
MubarakXBT <kazeemmubarak@gmail.com>
2023-12-13 16:38:47 +0100

    public method display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,9 @@ class Rectangle(Base):
     def area(self):
         """ area method that return area value of the retangle"""
         return (self.__width * self.__height)
+    
+    def display(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#",end='')
+            print()
