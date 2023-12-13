8f5c53452d015006dbe28fa67c3c9bba2bc2a820
MubarakXBT <kazeemmubarak@gmail.com>
2023-12-13 12:59:48 +0100

    Task 1-2: Class Base and Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,55 +12,39 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Object instantation"""
         super().__init__(id)
-        self.__height = height
-        self.__width = width
-        self.__x = x
-        self.__y = y
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
-        return (self.__width)
+        return self.__width
 
     @width.setter
-    def width(self):
-        return (self.__width)
-
-    @width.getter
-    def width(self):
-        return (self.__width)
+    def width(self, value):
+        self.__width = value
 
     @property
     def height(self):
-        return (self.__height)
+        return self.__height
 
     @height.setter
-    def height(self):
-        return (self.__height)
-
-    @height.getter
-    def height(self):
-        return (self.__height)
+    def height(self, value):
+        self.__height = value
 
     @property
     def x(self):
-        return (self.__x)
+        return self.__x
 
     @x.setter
-    def width(self):
-        return (self.__x)
-
-    @x.getter
-    def width(self):
-        return (self.__x)
+    def width(self, value):
+        self.__x = value
 
     @property
     def y(self):
-        return (self.__y)
+        return self.__y
 
     @y.setter
-    def width(self):
-        return (self.__y)
-
-    @y.getter
-    def width(self):
-        return (self.__y)
+    def width(self, value):
+        self.__y = value
