502ac65c34fe9ee30e9b15a76ecce7acc9c42561
MubarakXBT <kazeemmubarak@gmail.com>
2023-12-13 13:17:57 +0100

    Task 1-2: Class Base and Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,8 +11,8 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Object instantation"""
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
