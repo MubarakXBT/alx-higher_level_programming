67f4e9a23dc4f607f48bb9922ebc15a88beb8b6c
MubarakXBT <kazeemmubarak@gmail.com>
2023-12-14 08:25:02 +0100

    public method def update(self, *args, **kwargs) that assigns attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,3 +31,37 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ A method to assigns attributes
+        Args:
+            1st argument should be the id attribute
+            2nd argument should be the size attribute
+            3rd argument should be the x attribute
+            4th argument should be the y attribute
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                if a == 1:
+                    self.size = arg
+                if a == 2:
+                    self.x = arg
+                if a == 3:
+                    self.y = arg
+                a += 1
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'size':
+                    self.size = value
+                if key == 'x':
+                    self.x = value
+                if key == 'y':
+                    self.y = value
