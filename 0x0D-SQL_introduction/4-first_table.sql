2d3f8115baeaf9eeeeb51b61a2b4a91b37eb79ca
MubarakXBT <kazeemmubarak@gmail.com>
2024-01-18 15:49:24 +0100

    Create table in a database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a table called first_table with values id and name in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
