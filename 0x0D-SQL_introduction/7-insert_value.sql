65dff4ea519693067df1552dcd3c02e44d640bba
root <kazeemmubarak@gmail.com>
2024-01-24 10:04:57 +0000

    Task 6-7: to list value and insert in a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- INSERT INTO THE TABLE
-INSERT INTO `first table` (`id`, `name`) VALUES (89, "Best Schol");
+-- Insert to table.
+INSERT INTO `first_table` (`id`, `name`)
+VALUES (89, "Best Schol");
