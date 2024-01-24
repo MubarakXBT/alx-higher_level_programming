4db39eed2075b56fc59ba5d0de4cbcb23ffc824d
root <kazeemmubarak@gmail.com>
2024-01-24 12:51:19 +0000

    Task 6-7: to list value and insert in a table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- create and fill a table wih attributes id, name, and score in sql server
-CREATE TABLE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score) VALUES (1, "Bob", 14);
