4e7823b77b082338022199c910c1a56e7372ddec
MubarakXBT <kazeemmubarak@gmail.com>
2024-01-26 07:58:15 +0100

    fix task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- list all record in table second_table with score>= 10 in descending order
-SELECT `score`, `name`
 FROM `score_table`
+SELECT `score`, `name`
 WHERE `score` >= 10
 ORDER BY `score` DESC;
